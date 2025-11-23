5235026a52bc1959e7bf5c703028ebee6fc5cd89
HariomArka <arkaghosh9832@gmail.com>
2025-11-23 11:43:54 +0530

    Arka Here

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -69,7 +69,7 @@ const Register = () => {
 
     setIsLoading(true)
     try {
-      const response = await fetch(`${REACT_APP_API_URL}/api/auth/register`, {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/auth/register`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
